c9cafb08a3d34e260a97984e3f2c38a4e71939bb
TonyKag <tonykag@mail.com>
2015-10-21 21:34:00 -0700

    chrome & firefox drivers added

--- src/test/java/com/sqa/ao/selenium/intro/BasicDriverRun.java
+++ src/test/java/com/sqa/ao/selenium/intro/BasicDriverRun.java
@@ -1,19 +1,31 @@
 package com.sqa.ao.selenium.intro;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class BasicDriverRun {
-	@BeforeClass
-	public void beforeClass() {
-		WebDriver driver = new FirefoxDriver();
-		driver.get("https://google.com");
-	}
+
+	private static String baseUrl;
+	private static WebDriver driver;
 
 	@Test
-	public void f() {
+	public void login() {
+		baseUrl = "http://google.com";
+		driver.get(baseUrl);
+	}
+
+	@BeforeClass
+	public void setupChrome() {
+		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@BeforeClass(enabled = false)
+	public void setupFirefox() {
+		driver = new FirefoxDriver();
 	}
 
 }
