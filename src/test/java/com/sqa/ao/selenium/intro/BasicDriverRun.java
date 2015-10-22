35b76fc2e2f9df0ad8ce710112319b83cab8db0a
TonyKag <tonykag@mail.com>
2015-10-21 21:48:37 -0700

    opera added

--- src/test/java/com/sqa/ao/selenium/intro/BasicDriverRun.java
+++ src/test/java/com/sqa/ao/selenium/intro/BasicDriverRun.java
@@ -3,6 +3,7 @@ package com.sqa.ao.selenium.intro;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -17,7 +18,7 @@ public class BasicDriverRun {
 		driver.get(baseUrl);
 	}
 
-	@BeforeClass
+	@BeforeClass(enabled = false)
 	public void setupChrome() {
 		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
@@ -26,6 +27,12 @@ public class BasicDriverRun {
 	@BeforeClass(enabled = false)
 	public void setupFirefox() {
 		driver = new FirefoxDriver();
+	}
+
+	@BeforeClass
+	public void setupOpera() {
+		System.setProperty("webdriver.opera.driver", "drivers\\operadriver.exe");
+		driver = new OperaDriver();
 	}
 
 }
