554b58f015f9d0ed440a1bd8a84fb87abc70d7be
TonyKag <tonykag@mail.com>
2015-10-22 18:32:31 -0700

    selenium IDE start

--- src/test/java/com/sqa/ao/selenium/intro/BasicDriverRun.java
+++ src/test/java/com/sqa/ao/selenium/intro/BasicDriverRun.java
@@ -1,9 +1,11 @@
 package com.sqa.ao.selenium.intro;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -11,6 +13,11 @@ public class BasicDriverRun {
 
 	private static String baseUrl;
 	private static WebDriver driver;
+
+	private static boolean isSupportedPlatform() {
+		Platform current = Platform.getCurrent();
+		return Platform.MAC.is(current) || Platform.WINDOWS.is(current);
+	}
 
 	@Test
 	public void login() {
@@ -29,10 +36,20 @@ public class BasicDriverRun {
 		driver = new FirefoxDriver();
 	}
 
-	@BeforeClass
+	@BeforeClass(enabled = false)
 	public void setupOpera() {
 		System.setProperty("webdriver.opera.driver", "drivers\\operadriver.exe");
 		driver = new OperaDriver();
+	}
+
+	@BeforeClass
+	public void setupSafari() {
+		System.setProperty("webdriver.safari.driver", "drivers\\SafariDriver.safariextz");
+		// System.setProperty("webdriver.safari.noinstall", "true"); // To stop
+		// uninstall
+		// each time
+
+		driver = new SafariDriver();
 	}
 
 }
